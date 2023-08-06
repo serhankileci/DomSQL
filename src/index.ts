ee17b5f1f7b29db30342fe1c1cf43daf0d0ec470
serhankileci <serhanselimkileci@gmail.com>
2023-08-07 02:10:41 +0300

    toggle system for the elements getter to make sure it only runs once per query

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 interface DomSQL {
-	readonly elements: () => Readonly<HTMLElement[]>;
 	select: (selector: string) => DomSQL;
 	where: (condition: (element: HTMLElement) => boolean) => DomSQL;
 	update: (cb: (element: HTMLElement) => void) => DomSQL;
@@ -8,6 +7,7 @@ interface DomSQL {
 	order: (compare: (a: HTMLElement, b: HTMLElement) => number) => DomSQL;
 	limit: (count: number) => DomSQL;
 	offset: (count: number) => DomSQL;
+	readonly elements: () => Readonly<HTMLElement[]>;
 }
 
 function DomSQL(): DomSQL {
@@ -16,18 +16,25 @@ function DomSQL(): DomSQL {
 		limitCount: number;
 		offsetCount: number;
 		rehydrateElements: () => void;
+		wasRehydratedToggle: boolean;
 	} = {
 		elements: [],
 		limitCount: Infinity,
 		offsetCount: 0,
+		wasRehydratedToggle: false,
 		rehydrateElements: () => {
-			if (privateState.limitCount || privateState.offsetCount) {
+			if (
+				!privateState.wasRehydratedToggle &&
+				(privateState.limitCount || privateState.offsetCount)
+			) {
 				const { limitCount, offsetCount } = privateState;
 
 				privateState.elements = privateState.elements.slice(
 					offsetCount,
 					offsetCount + limitCount
 				);
+
+				privateState.wasRehydratedToggle = true;
 			}
 		},
 	};
@@ -39,34 +46,42 @@ function DomSQL(): DomSQL {
 		},
 		select: function (selector) {
 			privateState.elements = Array.from(document.querySelectorAll(selector));
+			privateState.wasRehydratedToggle = false;
 			return this;
 		},
 		where: function (condition) {
 			privateState.elements = privateState.elements.filter(element => condition(element));
+			privateState.wasRehydratedToggle = false;
 			return this;
 		},
 		update: function (cb) {
 			privateState.elements.forEach(element => cb(element));
+			privateState.wasRehydratedToggle = false;
 			return this;
 		},
 		remove: function () {
 			privateState.elements.forEach(element => element.parentNode?.removeChild(element));
+			privateState.wasRehydratedToggle = false;
 			return this;
 		},
 		clear: function () {
 			privateState.elements = [];
+			privateState.wasRehydratedToggle = false;
 			return this;
 		},
 		order: function (compare) {
 			privateState.elements.sort(compare);
+			privateState.wasRehydratedToggle = false;
 			return this;
 		},
 		limit: function (count) {
 			privateState.limitCount = count;
+			privateState.wasRehydratedToggle = false;
 			return this;
 		},
 		offset: function (count) {
 			privateState.offsetCount = count;
+			privateState.wasRehydratedToggle = false;
 			return this;
 		},
 	};
