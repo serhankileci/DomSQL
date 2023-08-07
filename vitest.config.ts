4a4431b68731cb4638790a8683d625206e734fe1
Serhan Kileci <serhanselimkileci@gmail.com>
2023-08-07 14:47:09 +0300

    using istanbul for coverage, reporting via text instead of writing to file

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		environment: "jsdom",
+		coverage: {
+			provider: "istanbul",
+			reporter: ["text"],
+		},
 	},
 });
